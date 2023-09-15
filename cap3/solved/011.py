2ce455abb1d625915c48c02825de91c3d5f520f3
alissonjc <alisson.cavalcante94@gmail.com>
2023-09-15 16:18:06 -0300

    solved 011

---
+++ cap3/solved/011.py
@@ -0,0 +1,11 @@
+number = float(input())
+
+square = number**2
+
+cube = number**3
+
+square_root = number**(1/2)
+
+cube_root = number**(1/3)
+
+print(f"Square: {square:.2f}, Cube: {cube:.2f}, Square Root: {square_root:.2f}, Cube Root: {cube_root:.2f}")
